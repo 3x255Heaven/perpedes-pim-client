3a8ad76736ffe639bc6c4e4642a10a74671c8966
Q <isakkovicq@gmail.com>
2025-03-19 19:14:08 +0100

    Layout

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import Layout from "./layouts/layout";
+
+function App() {
+  return (
+    <div className="flex flex-col items-center justify-center min-h-svh">
+      <Layout>
+        <></>
+      </Layout>
+    </div>
+  );
+}
+
+export default App;
